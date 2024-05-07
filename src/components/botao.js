07f4f071b06bee63bf45afed1db95f9cdff971e5
Wellington Correia Borsato Jr. <wellington.borsato@yahoo.com.br>
2024-05-07 14:01:54 -0300

    FaceLogin

---
+++ src/components/botao.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { TouchableOpacity, Text } from "react-native";
+import { styles } from "../css/botaoStyle";
+import { router } from "expo-router";
+
+export default function Botao({btn, cor, src}){
+    const [isPressed, setIsPressed] = useState(false);
+    const handlePress = () => {
+        setIsPressed(!isPressed);
+        router.navigate(src);
+    }
+
+    return (
+        <TouchableOpacity
+            style={[styles.button, { backgroundColor:cor }, isPressed ? styles.buttonPressed : null]}
+            onPress={handlePress}
+        >
+            <Text style={styles.buttonText}>{btn}</Text>
+        </TouchableOpacity>
+    );
+}
---
+++ src/css/botaoStyle.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    button: {
+     borderWidth:1,
+     borderColor:'#59606B',
+     paddingVertical: 20,
+     paddingHorizontal: 40,
+     borderRadius: 5,
+     marginVertical: 10,
+    },
+    buttonPressed: {
+        backgroundColor: '#0056b3'
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
+        textAlign: 'center',
+    }
+  });
+  
